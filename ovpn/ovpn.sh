1003f1ad351c2c1d0d47e8290ca5e9d010e6bbe0
Dambaev Alexander <ice.redmine@gmail.com>
2018-10-31 16:16:12 +0800

    1. define icmp type for icmp forwarding; 2. provide simple diagnostic at the end of the script

--- ovpn/ovpn.sh
+++ ovpn/ovpn.sh
@@ -45,7 +45,7 @@ GATEWAY=${LOCALIP%.*}.1
 iptables -t nat -N VPNFWDDNAT || true
 iptables -t nat -A VPNFWDDNAT -i tun0 -p tcp -m tcp -j DNAT --to-destination $FWD_IP
 iptables -t nat -A VPNFWDDNAT -i tun0 -p udp -m udp -j DNAT --to-destination $FWD_IP
-iptables -t nat -A VPNFWDDNAT -i tun0 -p icmp -m icmp -j DNAT --to-destination $FWD_IP
+iptables -t nat -A VPNFWDDNAT -i tun0 -p icmp --icmp-type any -m icmp -j DNAT --to-destination $FWD_IP
 
 
 iptables -t nat -N VPNFWDSNAT || true
@@ -58,4 +58,6 @@ iptables -t nat -A PREROUTING  -j VPNFWDDNAT
 
 #now tell gateway which device had been connected, so we can track it
 wget http://$GATEWAY/sstp-vpn/?uuid="$RESIN_DEVICE_UUID" || true
+
+ip addr show dev tun0 && echo "vpn tunnel looks working" || echo "vpn tunnel looks not working..."
 
